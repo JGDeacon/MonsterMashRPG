16ae9bb95d5d30a1cef2f99183eb16e6f85c8ef8
ShirishaB88 <shirisha46@gmail.com>
2021-04-15 14:27:40 -0400

    Adding Character Controller

--- CharacterCreatorServices/CharacterService.cs
+++ CharacterCreatorServices/CharacterService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CharacterCreatorData;
+using CharacterCreatorModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +15,73 @@ namespace CharacterCreatorServices
         public CharacterService(Guid userId)
         {
             _userId = userId;
+        }
+
+        public bool CreateCharacter(CharacterCreate model)
+        {
+            var entity =
+                new Character()
+                {
+                    User = _userId,
+                    Name = model.Name,
+                    BaseHP = model.HP,
+                    BaseSpd = model.SPD,
+                    BaseStr = model.STR
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Character.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<CharacterListItem> GetCharacters()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Character
+                    .Where(e => e.User == _userId)
+                    .Select(
+                        e =>
+                        new CharacterListItem
+                        {
+                            ID = e.ID,
+                            Name = e.Name,
+                            HP = e.BaseHP,
+                            SPD = e.BaseSpd,
+                            STR = e.BaseStr
+
+                        });
+
+                return query.ToArray();
+
+                        
+            }
+
+        }
+
+        public CharacterDetail GetCharacterById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                      ctx
+                        .Character
+                        .Single(e => e.ID == id && e.User == _userId);
+
+                return
+                    new CharacterDetail
+                    {
+                        ID = entity.ID,
+                        Name = entity.Name,
+                        HP = entity.BaseHP,
+                        SPD = entity.BaseHP,
+                        STR = entity.BaseStr
+                    };
+            }
         }
 
     }
