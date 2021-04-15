5877d07618f0b5a226a69ef4af04254fb094bd1f
JGDeacon <79653148+JGDeacon@users.noreply.github.com>
2021-04-14 21:40:51 -0400

    Created Character Class

--- CharacterCreatorData/Character.cs
+++ CharacterCreatorData/Character.cs
@@ -9,6 +9,19 @@ namespace CharacterCreatorData
 {
     public class Character
     {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public Guid User { get; set; }
+        [Required,MinLength(4),MaxLength(16)]
+        public string Name { get; set; }
+        [Required,Range(5,10,ErrorMessage ="Valid enteries are int between 5 & 10")]
+        public int BaseHP { get; set; }
+        [Required, Range(5, 10, ErrorMessage = "Valid enteries are int between 5 & 10")]
+        public int BaseStr { get; set; }
+        [Required, Range(5, 10, ErrorMessage = "Valid enteries are int between 5 & 10")]
+        public int BaseSpd { get; set; }
+        public List<Item> Inventory { get; set; }
 
     }
 }
