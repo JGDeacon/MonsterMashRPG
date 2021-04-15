6a3ac91af527a6caeebebb6ca93df71a0d8dc412
JGDeacon <79653148+JGDeacon@users.noreply.github.com>
2021-04-14 21:55:07 -0400

    Created LevelTable

--- CharacterCreatorData/Character.cs
+++ CharacterCreatorData/Character.cs
@@ -21,6 +21,7 @@ namespace CharacterCreatorData
         public int BaseStr { get; set; }
         [Required, Range(5, 10, ErrorMessage = "Valid enteries are int between 5 & 10")]
         public int BaseSpd { get; set; }
+        public int Level { get; set; }
         public Item InventoryOne { get; set; }
         public Item InventoryTwo { get; set; }
         public Item InventoryThree { get; set; }
---
+++ CharacterCreatorData/LevelTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterCreatorData
+{
+    public class LevelTable
+    {
+        [Key]
+        public int Level { get; set; }
+        public int XP { get; set; }
+    }
+}
